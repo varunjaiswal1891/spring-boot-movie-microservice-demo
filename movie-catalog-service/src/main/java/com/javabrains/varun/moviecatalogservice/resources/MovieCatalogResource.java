b2c1f18dc3dcbb9f3b852b46e5fda1d93a885e61
varunjaiswal1891 <varunpandey511@gmail.com>
2023-05-22 19:19:38 +0530

    Tested WebClient inplace of RestTemplate 22 May

--- movie-catalog-service/src/main/java/com/javabrains/varun/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/javabrains/varun/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.javabrains.varun.moviecatalogservice.models.CatalogItem;
 import com.javabrains.varun.moviecatalogservice.models.Movie;
@@ -20,6 +21,9 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
     {
@@ -31,7 +35,19 @@ public class MovieCatalogResource {
         //for each movieID call movie Info Service and get details
         //put them all together
         return ratingList.stream().map( r1 -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+r1.getMovieId(), Movie.class);
+            
+        //Use of RestTemplate which is going to be deprecated in future - using WebClient in future
+        Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+r1.getMovieId(), Movie.class);
+            
+           //Use of WebClient to call any RestAPI   -- use of Async programming 
+           /* 
+           Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/"+r1.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();   // this block() call is waiting untill this asynchronous call does not return response          
+            */
             return new CatalogItem(movie.getMovieName(),"test desccription11",r1.getRating());
         })
         .collect(Collectors.toList());
--- ratings-data-service/src/main/java/com/javabrains/varun/ratingsdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/com/javabrains/varun/ratingsdataservice/resources/RatingResource.java
@@ -1,5 +1,8 @@
 package com.javabrains.varun.ratingsdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +18,17 @@ public class RatingResource {
     {
         return new Rating(movieId, 6);
     }
+
+    @RequestMapping("/users/{userId}")
+    public List<Rating> getUserRating(@PathVariable("userId") String userId)
+    {
+         //get all rated movies first
+        List<Rating> ratingList =  Arrays.asList(
+                                        new Rating("1234", 3),
+                                        new Rating("5678", 5)
+                                    );
+        return ratingList;
+    }
+
+    
 }
